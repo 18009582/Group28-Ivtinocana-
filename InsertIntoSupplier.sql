2bccb6e224c11530d3d0ba4d3f3ae5eccb5ad5fa
NBungela <47412749+NBungela@users.noreply.github.com>
2020-09-03 04:39:01 +0200

    Add files via upload

---
+++ InsertIntoSupplier.sql
@@ -0,0 +1,10 @@
+USE Vehlution3
+GO
+
+INSERT INTO SUPPLIER
+(NAME_, CELL_NUMBER_, EMAIL_, ADDRESS)
+VALUES
+('Makel Parts', '0868963256', 'MakelParts@gmail.com', '785 Du Plooy Str'),
+('Motor Parts', '0856414325', 'MotorParts@gmail.com', '75 Jon Plesy Str'),
+('AC Motors', '0726896325', 'MakelParts@gmail.com', '785 Du Plooy Str')
+GO
